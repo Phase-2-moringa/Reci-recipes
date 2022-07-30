ebac22a67b6e3f3e15466174103132890e2949ca
Miriam Anyango <minaoanya@gmail.com>
2022-07-30 12:17:33 +0300

    Remove the db.json file from the src folder and in the App.js file create a h3 element with text inside

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Recipe from './Components/Recipe';
 function App() {
   return (
     <div className="container">
-      
+      <h3>The door to best and tasty meals is <br />
+          good recipe!</h3>
     </div>
   );
 }
